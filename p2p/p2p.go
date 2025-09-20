61082fb4a65a2cae5671be0b4316a6fdfa87b004
rockandcode4 <shrestharbn@gmail.com>
2025-09-20 14:11:34 +0545

    Update p2p.go

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,49 +1,55 @@
 package p2p
 
 import (
-    "context"
-    "fmt"
-    "log"
+	"context"
+	"log"
 
-    libp2p "github.com/libp2p/go-libp2p"
-    host "github.com/libp2p/go-libp2p/core/host"
-    peer "github.com/libp2p/go-libp2p/core/peer"
-    golog "github.com/ipfs/go-log/v2"
+	libp2p "github.com/libp2p/go-libp2p"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	host "github.com/libp2p/go-libp2p/core/host"
+	golog "github.com/ipfs/go-log/v2"
 )
 
 var logger = golog.Logger("p2p")
 
 type P2P struct {
-    ctx  context.Context
-    host host.Host
+	ctx  context.Context
+	host host.Host
+	ps   *pubsub.PubSub
 }
 
 func NewP2P(ctx context.Context, bind string) (*P2P, error) {
-    h, err := libp2p.New()
-    if err != nil { return nil, err }
-    p := &P2P{ctx: ctx, host: h}
-    return p, nil
+	h, err := libp2p.New()
+	if err != nil {
+		return nil, err
+	}
+	ps, err := pubsub.NewGossipSub(ctx, h)
+	if err != nil {
+		return nil, err
+	}
+	p := &P2P{ctx: ctx, host: h, ps: ps}
+	return p, nil
 }
 
 func (p *P2P) Start() {
-    addrs := p.host.Addrs()
-    pid := p.host.ID()
-    log.Printf("libp2p started: id=%s addrs=%v\n", pid.Pretty(), addrs)
-    // In a real network you'd use DHT/Bootstrap peers and pubsub
+	addrs := p.host.Addrs()
+	pid := p.host.ID()
+	log.Printf("libp2p started: id=%s addrs=%v\n", pid.Pretty(), addrs)
+	// create default topics for blocks/tx
+	_, err := p.ps.Join("graphene-blocks")
+	if err != nil {
+		logger.Error(err)
+	}
+	_, err = p.ps.Join("graphene-tx")
+	if err != nil {
+		logger.Error(err)
+	}
 }
 
 func (p *P2P) Stop() {
-    _ = p.host.Close()
+	_ = p.host.Close()
 }
 
 func (p *P2P) HostID() string {
-    return p.host.ID().Pretty()
-}
-
-func (p *P2P) Connect(addr string) error {
-    pi, err := peer.Decode(addr)
-    if err != nil { return err }
-    _, err = p.host.Network().DialPeer(p.ctx, pi)
-    if err != nil { return fmt.Errorf("dial peer: %w", err) }
-    return nil
+	return p.host.ID().Pretty()
 }
