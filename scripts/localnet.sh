0ac9e0b7ce16cb8ea932f5c42a6bf7a31dce302b
rockandcode4 <shrestharbn@gmail.com>
2025-09-20 14:22:28 +0545

    Update localnet.sh

--- scripts/localnet.sh
+++ scripts/localnet.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-
-# quick localnet script (3 nodes) for testing
-BIN=./bin/node
-if [ ! -f "$BIN" ]; then
-echo "build first: make build"
-exit 1
-fi
-
+make build
 
 mkdir -p localnet/node1 localnet/node2 localnet/node3
 
+# start nodes in background
+./bin/node --datadir ./localnet/node1 --rpc 8545 &
+./bin/node --datadir ./localnet/node2 --rpc 8546 &
+./bin/node --datadir ./localnet/node3 --rpc 8547 &
 
-# st
+echo "Nodes started. Use /rpc endpoint on ports 8545/8546/8547"
