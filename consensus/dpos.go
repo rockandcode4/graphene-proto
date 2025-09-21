d5b12245f9e3e9ad313129078306e5521414926c
rockandcode4 <shrestharbn@gmail.com>
2025-09-21 13:58:09 +0545

    Update dpos.go

--- consensus/dpos.go
+++ consensus/dpos.go
@@ -15,6 +15,7 @@ type Validator struct {
 var Validators []Validator
 var Blockchain []*Block
 
+// InitGenesis creates the genesis block
 func InitGenesis() {
     genesis := NewBlock(0, "", "genesis", []byte("Genesis Block"))
     Blockchain = append(Blockchain, genesis)
@@ -23,6 +24,35 @@ func InitGenesis() {
     fmt.Println("Genesis block created:", genesis.Hash)
 }
 
+// LoadBlockchain reloads the chain from LevelDB
+func LoadBlockchain() error {
+    headHash, err := store.LoadHead()
+    if err != nil {
+        return fmt.Errorf("no chain found, need to create genesis: %v", err)
+    }
+
+    // Walk backwards from head block until genesis
+    hash := headHash
+    var chain []*Block
+    for {
+        block, err := store.LoadBlock(hash)
+        if err != nil {
+            return fmt.Errorf("failed to load block: %v", err)
+        }
+        chain = append([]*Block{block}, chain...) // prepend
+
+        if block.Height == 0 {
+            break
+        }
+        hash = block.PrevHash
+    }
+
+    Blockchain = chain
+    fmt.Println("Blockchain loaded, height:", len(Blockchain)-1)
+    return nil
+}
+
+// ProduceBlock makes a new block from a validator
 func ProduceBlock(validator Validator, data []byte) *Block {
     prev := Blockchain[len(Blockchain)-1]
     block := NewBlock(prev.Height+1, prev.Hash, validator.Address, data)
@@ -35,6 +65,7 @@ func ProduceBlock(validator Validator, data []byte) *Block {
     return block
 }
 
+// RunConsensus loops through validators to produce blocks
 func RunConsensus() {
     for {
         for _, v := range Validators {
