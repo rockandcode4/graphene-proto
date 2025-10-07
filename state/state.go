64685ca4e9583fe1970c4f6358100414b2679014
rockandcode4 <shrestharbn@gmail.com>
2025-10-07 18:09:25 +0545

    Update state.go

--- state/state.go
+++ state/state.go
@@ -1,63 +1,87 @@
 package state
 
 import (
-	"encoding/json"
-	"fmt"
-	"path/filepath"
+    "encoding/json"
+    "fmt"
 
-	"github.com/syndtr/goleveldb/leveldb"
+    "github.com/syndtr/goleveldb/leveldb"
+    "gfn/store"
 )
 
 type Account struct {
-	Address string `json:"address"`
-	Balance uint64 `json:"balance"`
-	Nonce   uint64 `json:"nonce"`
+    Address string  `json:"address"`
+    Balance float64 `json:"balance"`
 }
 
-type StateDB struct {
-	db *leveldb.DB
+var accounts = make(map[string]*Account)
+var db *leveldb.DB
+
+// Initialize the state (open DB reference)
+func InitState() {
+    db = store.GetDB()
+    if db == nil {
+        fmt.Println("state: no database found")
+        return
+    }
+    loadAccounts()
 }
 
-func NewStateDB(dir string) (*StateDB, error) {
-	path := filepath.Join(dir, "db")
-	d, err := leveldb.OpenFile(path, nil)
-	if err != nil {
-		return nil, err
-	}
-	return &StateDB{db: d}, nil
+func loadAccounts() {
+    iter := db.NewIterator(nil, nil)
+    for iter.Next() {
+        key := string(iter.Key())
+        if len(key) > 4 && key[:4] == "acc:" {
+            var acc Account
+            json.Unmarshal(iter.Value(), &acc)
+            accounts[acc.Address] = &acc
+        }
+    }
+    iter.Release()
+    fmt.Printf("Loaded %d accounts\n", len(accounts))
 }
 
-func (s *StateDB) Close() error { return s.db.Close() }
-
-func (s *StateDB) GetAccount(addr string) (*Account, error) {
-	b, err := s.db.Get([]byte("acct:"+addr), nil)
-	if err != nil {
-		// not found => zero account
-		return &Account{Address: addr, Balance: 0, Nonce: 0}, nil
-	}
-	var a Account
-	if err := json.Unmarshal(b, &a); err != nil {
-		return nil, err
-	}
-	return &a, nil
+func SaveAccount(acc *Account) error {
+    data, _ := json.Marshal(acc)
+    return db.Put([]byte("acc:"+acc.Address), data, nil)
 }
 
-func (s *StateDB) PutAccount(a *Account) error {
-	b, _ := json.Marshal(a)
-	return s.db.Put([]byte("acct:"+a.Address), b, nil)
+func GetBalance(addr string) float64 {
+    if acc, ok := accounts[addr]; ok {
+        return acc.Balance
+    }
+    return 0
 }
 
-func (s *StateDB) Transfer(from, to string, amount uint64) error {
-	fa, _ := s.GetAccount(from)
-	ta, _ := s.GetAccount(to)
-	if fa.Balance < amount {
-		return fmt.Errorf("insufficient balance")
-	}
-	fa.Balance -= amount
-	ta.Balance += amount
-	fa.Nonce += 1
-	if err := s.PutAccount(fa); err != nil {
-		return err
-	}
-	return s.PutAccount(ta)
+func Transfer(from, to string, amount float64) error {
+    if GetBalance(from) < amount {
+        return fmt.Errorf("insufficient balance")
+    }
+
+    // Deduct
+    accounts[from].Balance -= amount
+
+    // Add to receiver
+    if _, ok := accounts[to]; !ok {
+        accounts[to] = &Account{Address: to, Balance: 0}
+    }
+    accounts[to].Balance += amount
+
+    // Save both
+    SaveAccount(accounts[from])
+    SaveAccount(accounts[to])
+
+    fmt.Printf("Transferred %.2f GFN from %s to %s\n", amount, from, to)
+    return nil
+}
+
+func Credit(addr string, amount float64) {
+    if _, ok := accounts[addr]; !ok {
+        accounts[addr] = &Account{Address: addr, Balance: 0}
+    }
+    accounts[addr].Balance += amount
+    SaveAccount(accounts[addr])
+}
+
+func Accounts() map[string]*Account {
+    return accounts
 }
